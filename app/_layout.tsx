809bd65590057d0a4764d3166d283903b0b5f005
Gunakar Challa <gunakarchalla@gmail.com>
2026-03-23 21:37:33 +0100

    intermediate commit safe area

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,19 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../services/nativewindInterop";
 import "./global.css";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: "#0f172a" },
-        headerTintColor: "white",
-        headerTitleStyle: { fontWeight: "600" },
-        contentStyle: { backgroundColor: "#f8fafc" },
-      }}
-    />
+    <SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: "#0f172a" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontWeight: "600" },
+          contentStyle: { backgroundColor: "#f8fafc" },
+        }}
+      />
+    </SafeAreaProvider>
   );
 }
